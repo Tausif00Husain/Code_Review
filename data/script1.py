02b5706e615c6a833a7be1b8af2d646607ab257f
Tausif00Husain <tausifhusain2001@gmail.com>
2025-03-02 11:49:50 +0530

    Initial commit - LLaMA 3.2 Code Review MVP

---
+++ data/script1.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    x, y = 5, 3
+    print("Sum:", add(x, y))
+    print("Difference:", subtract(x, y))
